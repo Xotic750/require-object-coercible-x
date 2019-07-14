2c08b13cb1ff6605a3a5b169234ff2a80ee6ce63
Graham Fairweather <xotic750@gmail.com>
2019-07-14 23:37:23 +0200

    :bookmark: v2.0.5
    Auto build

--- dist/require-object-coercible-x.js
+++ dist/require-object-coercible-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-13T23:32:13.271Z",
+  "date": "2019-07-14T21:37:19.439Z",
   "describe": "",
   "description": "ES6-compliant shim for RequireObjectCoercible.",
   "file": "require-object-coercible-x.js",
   "hash": "544481937d499693db91",
   "license": "MIT",
-  "version": "2.0.4"
+  "version": "2.0.5"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
