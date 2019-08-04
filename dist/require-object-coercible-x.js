87104ff2bac0e97c9033cc7d910a952a00863d68
Graham Fairweather <xotic750@gmail.com>
2019-08-04 21:15:11 +0200

    Auto run

--- dist/require-object-coercible-x.js
+++ dist/require-object-coercible-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-08-04T13:57:38.429Z",
+  "date": "2019-08-04T19:15:06.775Z",
   "describe": "",
   "description": "ES6-compliant shim for RequireObjectCoercible.",
   "file": "require-object-coercible-x.js",
